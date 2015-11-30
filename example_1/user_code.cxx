3e275a6b24bd6d49cd6d9f6567129f929e05da25
Sandeep Dutta <sandeep@systemviewinc.com>
2015-11-30 14:58:00 -0800

    fixed the 16 byte problem with the sample code

--- example_1/user_code.cxx
+++ example_1/user_code.cxx
@@ -8,6 +8,7 @@ int main() {
 #endif
 
 char s[16] = "Echo Server... ";
+char junk[16];
 void process_tcp1(
 		char in1[256],
 		char out1[256]){     
@@ -15,4 +16,6 @@ void process_tcp1(
 		if (i < 16 ) out1[i] = s[i];
 		else out1[i] = in1[i-16];
 	}
+	for (int i = 0; i < 16; i++)
+		junk[i] = in1[240+i];
 }
