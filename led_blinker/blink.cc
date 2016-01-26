0b0a3e65233f27e5d666ea0bfdc8aed831fbc217
Sandeep Dutta <sandeep@systemviewinc.com>
2016-01-26 11:31:41 -0800

    led blinker

---
+++ led_blinker/blink.cc
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+#include <string.h>
+void blink_led(char command[128]) {
+	static int led = 0;
+
+	switch (led) {
+	case 0:
+		strcpy(command,"LED_0");
+		led++;
+		break;
+	case 1:
+		strcpy(command,"LED_1");
+		led++;
+		break;
+	case 2:
+		strcpy(command,"LED_2");
+		led++;
+		break;	
+	case 3:
+		strcpy(command,"LED_3");
+		led++;
+		break;
+	case 4:
+		strcpy(command,"LED_4");
+		led++;
+		break;
+	case 5:
+		strcpy(command,"LED_5");
+		led++;
+		break;
+	case 6:
+		strcpy(command,"LED_6");
+		led++;
+		break;
+	case 7:
+		strcpy(command,"LED_7");
+		led = 0;
+		break;
+	}
+}
