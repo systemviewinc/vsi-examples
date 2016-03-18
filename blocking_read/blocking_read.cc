ef1794ca7cc425a7def5eac9e9f29cabed2d4cc8
Sandeep Dutta <sandeep@systemviewinc.com>
2016-03-18 13:18:21 -0700

    added blocking read

---
+++ blocking_read/blocking_read.cc
@@ -0,0 +1,15 @@
+#include "blocking_read.h"
+
+void blocking_read(hls::stream<ap_uint<8> > &in1,
+		   hls::stream<ap_uint<8> > &in2,
+		   hls::stream<ap_uint<8> > &outp) 
+{
+	ap_uint<8> in_1, in_2;
+	in_1 = in1.read();
+	if (in_1 == 'w') {
+		in_2 = in2.read();
+		outp.write(in_2);
+	} else {
+		outp.write(in_1);
+	}
+}
---
+++ blocking_read/blocking_read.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+#include <hls_stream.h>
+#include <ap_utils.h>
+#include "ap_axi_sdata.h"
+
+template<int D>
+struct ap_axis_dk {
+	ap_uint<D> data;
+	ap_uint<1> last;
+	ap_uint<D/8> keep;
+};
+template<int D>
+struct ap_axis_dkt {
+	ap_uint<D> data;
+	ap_uint<1> last;
+	ap_uint<D/8> keep;
+	ap_uint<1> id;
+};
