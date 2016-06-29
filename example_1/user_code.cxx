6cf9ed83147a6082b20c6fe01ccecb9bd4b6a5b7
Adnan <adnan@systemviewinc.com>
2016-06-29 13:26:55 -0700

    modified example 1

--- example_1/user_code.cxx
+++ example_1/user_code.cxx
@@ -1,14 +1,29 @@
 #include <string.h>
+#include <hls_stream.h>
 
-char s[16] = "Echo Server... ";
-char junk[16];
+char s[] = "Echo.";
+int s_len = sizeof(s) -1;
+
 void process_tcp1(
 		char in1[256],
-		char out1[256]){     
+		char out1[256]){
 	for(int i = 0; i < 256; i++) {
-		if (i < 16 ) out1[i] = s[i];
-		else out1[i] = in1[i-16];
+		if (i < s_len ) out1[i] = s[i];
+		else out1[i] = in1[i-s_len];
 	}
-	for (int i = 0; i < 16; i++)
-		junk[i] = in1[240+i];
+}
+
+void func1(int a[16], hls::stream<int> &b, hls::stream<int> &r) {
+	for(int i = 0; i < 16; i++ ) {
+		b.write(a[i]);
+	}
+	int j = r.read();
+}
+
+void func2(hls::stream<int> &a, hls::stream<int> &r) {
+	int _r = 0;
+	while(!a.empty()) {
+		_r += a.read();
+	}
+	r.write(_r);
 }
