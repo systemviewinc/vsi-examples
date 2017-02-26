6633bc06b4153cba525ff6466b63432b9f7aa467
Adnan <adnan@systemviewinc.com>
2017-02-25 20:08:16 -0800

    added hls stream example for java

--- example_1/user_code.java
+++ example_1/user_code.java
@@ -1,5 +1,6 @@
 import java.nio.*;
 import java.util.Arrays;
+import com.systemviewinc.runtime.*;
 
 class Main {
 	public static void process(ByteBuffer in, ByteBuffer out) {
@@ -11,5 +12,18 @@ class Main {
 		out.putChar('!');
 		out.putChar('.');
 		Utils.transfer(out, in);
+	}
+
+	public static void process_stream(StreamBuffer sbIn, StreamBuffer sbOut) {
+		ByteBuffer in = (ByteBuffer)sbIn.readByteBuffer();
+		Utils.printByteBuffer(in);
+		ByteBuffer out = Utils.makeDirectByteBuffer(256);
+		process(in, out);
+		Utils.printByteBuffer(out);
+		sbOut.writeByteBuffer(out);
+	}
+
+	public static void process_device(VsiDevice devIn, VsiDevice devOut) {
+
 	}
 }
