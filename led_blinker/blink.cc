f43abec45567a6878521efb3e37898975ed8ae37
Sandeep Dutta <sandeep@systemviewinc.com>
2017-06-07 10:30:02 -0700

    added function to driver led

--- led_blinker/blink.cc
+++ led_blinker/blink.cc
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <vsi_device.h>
 void blink_led(char command[128]) {
 	static int led = 0;
 	
@@ -38,4 +39,27 @@ void blink_led(char command[128]) {
 		led = 0;
 		break;
 	}
+}
+
+void drive_led(char command[128], vsi::device &led)
+{
+	unsigned int led_val = 0;
+	if (strcmp(command,"LED_0") == 0) {
+		led_val = 1;
+	} else 	if (strcmp(command,"LED_1") == 0) {
+		led_val = 2;
+	} else 	if (strcmp(command,"LED_2") == 0) {
+		led_val = 4;
+	} else 	if (strcmp(command,"LED_3") == 0) {
+		led_val = 8;
+	} else 	if (strcmp(command,"LED_4") == 0) {
+		led_val = 16;
+	} else	if (strcmp(command,"LED_5") == 0) {
+		led_val = 32;
+	} else 	if (strcmp(command,"LED_6") == 0) {
+		led_val = 64;
+	} else 	if (strcmp(command,"LED_7") == 0) {
+		led_val = 128;
+	}
+	led.pwrite(&led_val,sizeof(led_val),0);
 }
