8d3b48bef3571931137d860e0f9af536308d2a69
Sandeep Dutta <sandeep@systemviewinc.com>
2017-07-06 06:52:12 -0700

    added tests to readfrom file & send

--- memory/memory.cc
+++ memory/memory.cc
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 #include "mem_test.h"
-
+#ifndef __VSI_HLS_SYN__
 /** 
  * @brief write a patter to memory and tell another process when write is
  * 	  complete and wait for ack from the other process.
@@ -62,17 +63,66 @@ void mem_read(vsi::device &mem, hls::stream<int> &ctl_in, hls::stream<int> &ctl_
 		else offset += 4096;
 	}
 }
+#endif
+/** 
+ * @brief read from a file and write to a stream then go to sleep
+ * 
+ * @param out_s : output stream
+ */
+void read_file_send(hls::stream<int> &out_s)
+{
+	// read from a file & write to a stream
+	FILE *f;
+	if ((f = fopen("./infile.txt","r")) == NULL) {
+		perror("Can not open inputfile 'infile.txt'\n");		
+	}
+	printf("Starting read & send\n");
+	while (!feof(f)) {
+		int fdata, rv;
+		if ((rv = fread(&fdata,sizeof(fdata),1,f)) == 1) {
+			out_s.write(fdata);
+		}
+	}
+	printf("closing \n");
+	fclose(f);
+        printf("done read_send going to sleep");
+	while(1) sleep(10);
+}
 
-void mem_read_write(vsi::device &mem) {
-	char val[4096] ;
-	char wval = 'a';
-	int offset = 0 ;
-	while (1) {
-		mem.pwrite(val,sizeof(val),offset); // write to  offset
-		if (wval != 'z') wval++;
-		else wval = 'a';
-		if (offset == (1024*1024*1024)) offset = 0;
-		else offset += 4096;
-		sleep(1);
-	}	
+/** 
+ * @brief read from a stream & write to a file
+ * 
+ * @param in_s : input stream
+ */
+void recv_write_file(hls::stream<int> &in_s)
+{
+	FILE *f;
+	if ((f = fopen("./outfile.txt","w")) == NULL) {
+		perror("Can not open outputfile 'outfile.txt'\n");		
+	}
+	while(1) {
+		// wait for data to arrive & write to file
+		int fdata = in_s.read() ; // blocking read
+		fwrite(&fdata,sizeof(fdata),1,f);
+		fflush(f);		
+	}
+}
+
+/** 
+ * @brief process data in batches of 16 integers
+ * 
+ * @param in_s 
+ * @param out_s 
+ */
+void process_data(hls::stream<int> & in_s, hls::stream<int> & out_s) {
+	// process in chunks of 16 integers
+	int fdata[16];
+	printf(" gathering 16 integers\n");
+	for (int i = 0 ; i < sizeof(fdata)/sizeof(fdata[0]); i++) {
+		fdata[i] = in_s.read();
+	}
+	printf(" write them to the output\n");
+	for (int i = 0 ; i < sizeof(fdata)/sizeof(fdata[0]); i++) {
+		out_s.write(fdata[i]);
+	}
 }
