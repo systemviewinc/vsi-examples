cc42d64384c92bde22dff060d73528bd488cc25a
matt <matt@systemviewinc.com>
2018-07-16 11:56:57 -0700

    fix for shared memory type

--- hls_master/hls_master.cc
+++ hls_master/hls_master.cc
@@ -4,7 +4,7 @@
 
 void hls_master (vsi::device &mem)
 {
-	int buff[10];
-	for (int i = 0 ; i < 10; i++) buff[i] = i;
+	char buff[10*4];
+	for (int i = 0 ; i < 10*4; i++) buff[i] = i;
 	mem.pwrite(buff,sizeof(buff),0);
 }
