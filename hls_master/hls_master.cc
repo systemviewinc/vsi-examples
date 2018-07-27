8a19173960b5c9c931bd65e0322800b08974fbf0
matt <matt@systemviewinc.com>
2018-07-27 11:36:41 -0700

    added a shared memory reader

--- hls_master/hls_master.cc
+++ hls_master/hls_master.cc
@@ -8,3 +8,15 @@ void hls_master (vsi::device &mem)
 	for (int i = 0 ; i < 10*4; i++) buff[i] = i;
 	mem.pwrite(buff,sizeof(buff),0);
 }
+
+
+void hls_reader (vsi::device &mem)
+{
+	char buff[10*4];
+	mem.pread(buff,sizeof(buff),0);
+	printf("-----READ BUFFER-----\n");
+	for (int i = 0 ; i < 10*4; i++)
+		printf("%d : %d \n", i, buff[i]);
+	printf("---------------------\n");
+
+}
