015bedce30b00cfa6f4f6442835071dd0a8dbb60
Adnan <adnan@systemviewinc.com>
2018-08-30 16:35:41 -0700

    added passthrough device memory write for load on demand

--- pass_thru_r5/pass_thru.cc
+++ pass_thru_r5/pass_thru.cc
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <vsi_device.h>
+
+char s[] = "Echo.";
+int s_len = sizeof(s) -1;
+
 /**
  * @brief just a pass through
  *
@@ -11,5 +16,25 @@ void pass_thru_r5(int in_arr[1024], int out_arr[1024])
 	for (int i =0 ; i < 1024; i++)
 #pragma HLS PIPELINE II=1
 		out_arr[i] = in_arr[i];
+	printf("%s done\n",__FUNCTION__);
+}
+
+
+void pass_thru_device(char in_arr[1024], vsi::device &dev_out)
+{
+	printf("%s started\n",__FUNCTION__);
+	for (int i =0 ; i < 1024; i++)
+#pragma HLS PIPELINE II=1
+	dev_out.write(in_arr, sizeof(in_arr));
+	printf("%s done\n",__FUNCTION__);
+}
+
+void echo_pass_thru_device(char in_arr[1024], vsi::device &dev_out)
+{
+	printf("%s started\n",__FUNCTION__);
+	for (int i =0 ; i < 1024; i++)
+#pragma HLS PIPELINE II=1
+	dev_out.write(s, sizeof(s));
+	dev_out.write(in_arr, sizeof(in_arr));
 	printf("%s done\n",__FUNCTION__);
 }
