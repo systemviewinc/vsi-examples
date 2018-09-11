6c8e838ade27035cc871576b8a0ca020c10ccbc5
Adnan <adnan@systemviewinc.com>
2018-09-11 14:34:19 -0700

    added inverter

--- sort/sort.cc
+++ sort/sort.cc
@@ -1,6 +1,6 @@
 // An iterative implementation of quick sort
 #include <stdio.h>
- 
+
 // A utility function to swap two elements
 void swap ( int* a, int* b )
 {
@@ -8,13 +8,13 @@ void swap ( int* a, int* b )
     *a = *b;
     *b = t;
 }
- 
+
 /* This function is same in both iterative and recursive*/
 int partition (int arr[], int l, int h)
 {
     int x = arr[h];
     int i = (l - 1);
- 
+
     for (int j = l; j <= h- 1; j++)
     {
         if (arr[j] <= x)
@@ -26,30 +26,30 @@ int partition (int arr[], int l, int h)
     swap (&arr[i + 1], &arr[h]);
     return (i + 1);
 }
- 
+
 /* A[] --> Array to be sorted, l  --> Starting index, h  --> Ending index */
 void quickSortIterative (int arr[], int l, int h)
 {
     // Create an auxiliary stack
     int stack[ h - l + 1 ];
- 
+
     // initialize top of stack
     int top = -1;
- 
+
     // push initial values of l and h to stack
     stack[ ++top ] = l;
     stack[ ++top ] = h;
- 
+
     // Keep popping from stack while is not empty
     while ( top >= 0 )
     {
         // Pop h and l
         h = stack[ top-- ];
         l = stack[ top-- ];
- 
+
         // Set pivot element at its correct position in sorted array
         int p = partition( arr, l, h );
- 
+
         // If there are elements on left side of pivot, then push left
         // side to stack
         if ( p-1 > l )
@@ -57,7 +57,7 @@ void quickSortIterative (int arr[], int l, int h)
             stack[ ++top ] = l;
             stack[ ++top ] = p - 1;
         }
- 
+
         // If there are elements on right side of pivot, then push right
         // side to stack
         if ( p+1 < h )
@@ -74,4 +74,10 @@ void sort (int in_arr[1024], int out_arr[1024])
 	for (int i = 0 ; i < 1024; i++)
 		out_arr[i] = in_arr[i];
 	quickSortIterative(out_arr,0, 1023);
+}
+
+void inverter(int in_arr[1024], int out_arr[1024]) {
+    for (int i = 0 ; i < 1024; i++) {
+        out_arr[i] = 0xffffffff - in_arr[i];
+    }
 }
