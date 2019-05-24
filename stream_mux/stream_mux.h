7f9be3ee28950ac20d9f7ee70de67aef884ba191
sandeepdutta <sandeep.dutta@ieee.org>
2019-05-24 13:13:49 -0700

    changes to example code

--- stream_mux/stream_mux.h
+++ stream_mux/stream_mux.h
@@ -9,6 +9,7 @@ using namespace std;
 #include <hls_stream.h>
 #include <ap_utils.h>
 #include "ap_axi_sdata.h"
+#include <string.h>
 #ifndef __VSI_HLS_SYN__
 #include <vsi_device.h>
 #include <unistd.h>
