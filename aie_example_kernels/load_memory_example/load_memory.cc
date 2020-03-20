461b1862ed561aa71ab9166f903220bd447f563b
sandeepdutta <sandeep.dutta@ieee.org>
2020-03-20 10:18:22 -0700

    load memory example

---
+++ aie_example_kernels/load_memory_example/load_memory.cc
@@ -0,0 +1,58 @@
+#ifndef __GRAPH_H__
+#define __GRAPH_H__
+
+#include <cardano.h>
+#include "kernels.h"
+
+extern int32 lut0[6*1024];  // local memory for master
+extern void masterKernel(input_stream_int32 * s1,
+			 output_window_int32 *o0,
+			 output_window_int32 *o1,
+			 output_window_int32 *o2, 
+			 output_stream_int32 * os1);
+extern void slaveKernel(input_window_int32 * s1);
+
+class myGraph : public cardano::graph {
+ private:
+	cardano::kernel master;
+	cardano::kernel slaveS;
+	cardano::kernel slaveW;
+	cardano::kernel slaveN;
+	cardano::parameter l0; 
+public:
+	cardano::port<input> in;     // public interface to the graph
+	cardano::port<output> out;   // is streaming interface 
+	myGraph() {
+		master = cardano::kernel::create(masterKernel);
+		cardano::source(master) = "master.cc";
+		cardano::runtime<ratio>(master) = 0.9;
+		single_buffer(master.out[0]);  // double buffer will run out of space
+		single_buffer(master.out[1]);
+		single_buffer(master.out[2]);
+		l0 = cardano::parameter::array(lut0);
+		cardano::connect<>(l0,master);
+		slaveS = cardano::kernel::create(slaveKernel);
+		single_buffer(slaveS.in[0]);
+		cardano::source(slaveS) = "slave.cc";
+		slaveW = cardano::kernel::create(slaveKernel);
+		cardano::source(slaveW) = "slave.cc";
+		single_buffer(slaveW.in[0]);
+		slaveN = cardano::kernel::create(slaveKernel);
+		cardano::source(slaveN) = "slave.cc";
+		single_buffer(slaveN.in[0]);
+		cardano::runtime<ratio>(slaveS) = 0.9;
+		cardano::runtime<ratio>(slaveW) = 0.9;
+		cardano::runtime<ratio>(slaveN) = 0.9;
+		
+		// use of async keyword shows that lock primitives are 
+		// managed by the user.  Locks are still allocated by the compiler. 
+		
+		cardano::connect<cardano::stream> net0(in, master.in[0]);
+		cardano::connect<cardano::stream> net1(master.out[3], out);
+		cardano::connect<cardano::window<6*1024>> net2(async(master.out[0]), 
+							       async(slaveS.in[0]));
+		cardano::connect<cardano::window<6*1024>> net3(async(master.out[1]), async(slaveW.in[0]));
+		cardano::connect<cardano::window<6*1024>> net4(async(master.out[2]), async(slaveN.in[0]));
+	}
+};
+#endif
---
+++ aie_example_kernels/load_memory_example/master.cc
@@ -0,0 +1,99 @@
+#include <cardano.h>
+#define SZ (6*1024)
+int32 chess_storage(%chess_alignof(v4int32)) lut0[SZ]; 
+
+
+// Even though we allocate 24k buffer for data per core,
+// The example is hard coded for 16 samples, to 
+// keep the simulation time minimal and yet 
+// showing the communication and synchronization. 
+
+void masterKernel(input_stream_int32  *s1,
+                  output_window_int32 *o0,
+                  output_window_int32 *o1,
+                  output_window_int32 *o2, 
+                  output_stream_int32 *os1) {
+	
+	v4int32 *p = (v4int32 *) lut0; 
+	int i; 
+	acquire(o0->lockids[0],0);
+	o0->ptr=o0->head;
+	for (i=0;i< 16 ;i++) {
+		//     v4int32 data = readincr_v4(s1);
+		int32 data = readincr(s1);
+		window_writeincr(o0, data);
+	}
+	release(o0->lockids[0],1);
+	o0->ptr=o0->head;
+	o1->ptr=o1->head;
+	acquire(o1->lockids[0],0);
+	for (i=0;i< 16 ;i++) {
+		//v4int32 data = readincr_v4(s1);
+		int32 data = readincr(s1);
+		window_writeincr(o1, data);
+	}
+	o1->ptr=o1->head;
+	release(o1->lockids[0],1);
+	
+	acquire(o2->lockids[0],0);
+	o2->ptr=o2->head;
+	for (i=0;i< 16 ;i++) {
+		// v4int32 data = readincr_v4(s1);
+		int32 data = readincr(s1);
+		window_writeincr(o2, data);
+	}
+	o2->ptr=o2->head;
+	release(o2->lockids[0],1);
+	
+	//  p = (v4int32 *) lut0;
+	int32 *p0 = (int32 *) lut0;
+	for (i=0;i< 16 ;i++) {
+		// v4int32 data = readincr_v4(s1);
+		int32 data = readincr(s1);
+		*p0++ = data; 
+	}
+	
+	// wait for the slaves to finish
+	printf("\nWaiting for slaves to finish \n");
+	acquire(o0->lockids[0],0);
+	acquire(o1->lockids[0],0);
+	acquire(o2->lockids[0],0);
+	printf("\nSlaves are done \n");
+	release(o0->lockids[0],0);
+	release(o1->lockids[0],0);
+	release(o2->lockids[0],0);
+	
+	int32 *pi = (int32 *) o0->head;
+	
+	for (i=0;i< 16 ;i++) {
+		//    v4int32 data = *p++;
+		// writeincr_v4(os1, data); 
+		int32 data = *pi++;
+		writeincr(os1, data); 
+	}
+	
+	//  p = (v4int32 *) o1->head;
+	pi = (int32 *) o1->head;
+	for (i=0;i< 16 ;i++) {
+		//    v4int32 data = *p++;
+		// writeincr_v4(os1, data); 
+		int32 data = *pi++;
+		writeincr(os1, data); 
+	}
+	// p = (v4int32 *) o2->ptr; 
+	pi = (int32 *) o2->head;
+	for (i=0;i< 16 ;i++) {
+		//    v4int32 data = *p++;
+		//writeincr_v4(os1, data); 
+		int32 data = *pi++;
+		writeincr(os1, data); 
+	}
+	//  p = (v4int32 *) lut0; 
+	pi = (int32 *) lut0;
+	for (i=0;i< 16;i++) {
+		// v4int32 data = *p++;
+		// writeincr_v4(os1, data); 
+		int32 data = *pi++;
+		writeincr(os1, data); 
+	}
+}
