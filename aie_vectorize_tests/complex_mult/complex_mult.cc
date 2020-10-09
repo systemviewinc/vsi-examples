5fbb4b4f0dcc911f5ad474ac724e7aa0fff57486
kafi <kafi@melodrama.corp.systemviewinc.com>
2020-10-09 11:13:53 -0700

    Adding src & input files

--- aie_vectorize_tests/complex_mult/complex_mult.cc
+++ aie_vectorize_tests/complex_mult/complex_mult.cc
@@ -2,9 +2,9 @@
 
 
 void complex_mult(float * __restrict__ A, float * __restrict__ B, float * __restrict__ C) {
-	_complex * __restrict Ai = (_complex * __restrict__)A; 
-	_complex * __restrict Bi = (_complex * __restrict__)B; 
-	_complex * __restrict Co = (_complex * __restrict__)C;
+	_complex<float> * __restrict Ai = (_complex<float> * __restrict__)A; 
+	_complex<float> * __restrict Bi = (_complex<float> * __restrict__)B; 
+	_complex<float> * __restrict Co = (_complex<float> * __restrict__)C;
 	
 #pragma clang loop vectorize(enable) interleave_count(2)
 	for (int i = 0 ; i < 64; i++)
