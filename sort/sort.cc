d2072337c4e855e0756ea24fe79a166efb3172e7
Artem Marisov <artem@systemviewinc.com>
2020-12-18 10:17:39 -0800

    Using buffers allocation obviously as required in 2020.2 in sort example.

--- sort/sort.cc
+++ sort/sort.cc
@@ -26,44 +26,48 @@ int partition (int arr[], int l, int h)
     swap (&arr[i + 1], &arr[h]);
     return (i + 1);
 }
-
+#define BUF_SIZE 1024
 /* A[] --> Array to be sorted, l  --> Starting index, h  --> Ending index */
-void quickSortIterative (int arr[], int l, int h)
+void quickSortIterative (int arr[])
+// , int l, int h)
 {
-    // Create an auxiliary stack
-    int stack[ h - l + 1 ];
+    // Create an auxiliary bufstack_
+    // int bufstack_[ h - l + 1 ];
+    int bufstack_[ BUF_SIZE ];
+    int l = 0;
+    int h = BUF_SIZE - 1;
 
-    // initialize top of stack
+    // initialize top of bufstack_
     int top = -1;
 
-    // push initial values of l and h to stack
-    stack[ ++top ] = l;
-    stack[ ++top ] = h;
+    // push initial values of l and h to bufstack_
+    bufstack_[ ++top ] = l;
+    bufstack_[ ++top ] = h;
 
-    // Keep popping from stack while is not empty
+    // Keep popping from bufstack_ while is not empty
     while ( top >= 0 )
     {
         // Pop h and l
-        h = stack[ top-- ];
-        l = stack[ top-- ];
+        h = bufstack_[ top-- ];
+        l = bufstack_[ top-- ];
 
         // Set pivot element at its correct position in sorted array
         int p = partition( arr, l, h );
 
         // If there are elements on left side of pivot, then push left
-        // side to stack
+        // side to bufstack_
         if ( p-1 > l )
         {
-            stack[ ++top ] = l;
-            stack[ ++top ] = p - 1;
+            bufstack_[ ++top ] = l;
+            bufstack_[ ++top ] = p - 1;
         }
 
         // If there are elements on right side of pivot, then push right
-        // side to stack
+        // side to bufstack_
         if ( p+1 < h )
         {
-            stack[ ++top ] = p + 1;
-            stack[ ++top ] = h;
+            bufstack_[ ++top ] = p + 1;
+            bufstack_[ ++top ] = h;
         }
     }
 }
@@ -71,25 +75,25 @@ void quickSortIterative (int arr[], int l, int h)
 // Classic version, operates on out_arr, broken by HLS bug in Vivado 2019.2
 void sort_v1 (int in_arr[1024], int out_arr[1024])
 {
-	for (int i = 0 ; i < 1024; i++)
+	for (int i = 0 ; i < BUF_SIZE; i++)
 		out_arr[i] = in_arr[i];
-	quickSortIterative(out_arr,0, 1023);
+	quickSortIterative(out_arr);
 }
 
 // Version of sort developed to work around HLS bug in Vivado 2019.2
 void sort (int in_arr[1024], int out_arr[1024])
 {
 	int i;
-	int inter_arr[1024];
-	for (i = 0; i < 1024; i++)
+	int inter_arr[BUF_SIZE];
+	for (i = 0; i < BUF_SIZE; i++)
 		inter_arr[i] = in_arr[i];
-	quickSortIterative(inter_arr,0, 1023);
-	for (i = 0; i < 1024; i++)
+	quickSortIterative(inter_arr);
+	for (i = 0; i < BUF_SIZE; i++)
 		out_arr[i] = inter_arr[i];
 }
 
 void inverter(int in_arr[1024], int out_arr[1024]) {
-    for (int i = 0 ; i < 1024; i++) {
+    for (int i = 0 ; i < BUF_SIZE; i++) {
         out_arr[i] = 0xffffffff - in_arr[i];
     }
 }
