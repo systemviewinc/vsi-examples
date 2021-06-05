0b6498c5d9bc56c10527b05d20c518caf233040c
kafi <kafi@dogmatix.corp.systemviewinc.com>
2021-06-04 17:36:08 -0700

    Adding topFunction complex arguments jenkins tests

---
+++ aie_vectorize_tests/tf_cfloat_stream/tf_cfloat_stream.cc
@@ -0,0 +1,14 @@
+#include "_complex.h"
+
+
+void tf_cfloat_stream(_complex<float> * __restrict__ A, _complex<float> * __restrict__ B, _complex<float> * __restrict__ C0, _complex<float> * __restrict__ C1) {
+
+
+        #pragma clang loop vectorize(disable) //interleave_count(1)
+        for (int i = 0 ; i < 64; i++)
+            C0[i] =  A[i] + B[i];
+
+        #pragma clang loop vectorize(enable) //interleave_count(1)
+        for (int i = 0 ; i < 64; i++)
+            C1[i] =  A[i] + B[i];
+}
---
+++ aie_vectorize_tests/tf_cint32_window/tf_cint32_window.cc
@@ -0,0 +1,14 @@
+#include "_complex.h"
+
+
+void tf_cint32_window(_complex<int32_t> * __restrict__ A, _complex<int32_t> * __restrict__ B, _complex<int32_t> * __restrict__ C0, _complex<int32_t> * __restrict__ C1) {
+
+
+        #pragma clang loop vectorize(disable) //interleave_count(1)
+        for (int i = 0 ; i < 64; i++)
+            C0[i] =  A[i] + B[i];
+
+        #pragma clang loop vectorize(enable) //interleave_count(1)
+        for (int i = 0 ; i < 64; i++)
+            C1[i] =  A[i] + B[i];
+}
