fb2d7414fe2c6bfd2bf049cc03ac49fad6f936e3
kafi <kafi@dogmatix.corp.systemviewinc.com>
2021-06-11 12:10:17 -0700

    Adding stream jenkins test

---
+++ aie_vectorize_tests/stream_interleave2/stream_interleave2.cc
@@ -0,0 +1,14 @@
+#include <stdint.h>
+
+
+void stream_interleave2(int32_t * __restrict__ A, float * __restrict__ B,  int32_t * __restrict__ C0, float * __restrict__ C1) {
+
+    	#pragma clang loop vectorize(enable) interleave_count(2)
+			for (int i = 0 ; i < 64; i++)
+    			C0[i] =   A[i] * 2;
+
+    	#pragma clang loop vectorize(enable) interleave_count(2)
+			for (int i = 0 ; i < 64; i++)
+    			C1[i] =   B[i] * 2.0f;
+
+}
---
+++ aie_vectorize_tests/stream_whileLoop_v3/stream_whileLoop_v3.cc
@@ -0,0 +1,17 @@
+#include <stdint.h>
+
+
+static int32_t coeff_0[8] =  {1,2,3,4,5,6,7,8};
+static float coeff_1[8]   =  {2.0f,3.0f,4.0f,5.0f,6.0f,7.0f,8.0f,9.0f};
+
+void stream_whileLoop_v3(int32_t * __restrict__ A, float * __restrict__ B,
+		   int32_t * __restrict__ C0, float * __restrict__ C1) {
+
+         while(1) {
+            #pragma clang loop vectorize(disable)
+		        for (int i=0 ; i < 8; i++) {
+			               *C0++ = (*A++ * coeff_0[i]);
+			               *C1++ = (*B++ * coeff_1[i]);
+		        }
+	       }
+}
