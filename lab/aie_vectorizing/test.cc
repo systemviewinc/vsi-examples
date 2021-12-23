9d52326508353ec52553cb3f6f9bef051d519649
Somayeh Rahimipour <somayeh@tragicomix.corp.systemviewinc.com>
2021-12-22 18:48:14 -0800

    adding new test for auto_vec lab

--- lab/aie_vectorizing/test.cc
+++ lab/aie_vectorizing/test.cc
@@ -1,11 +1,25 @@
-static float K=3.1415f;
-void top (float A[1024] , 
-	  float B[1024] , 
-	  float C[1024]) { 
-//void top (float * __restrict__ A , 
-//	  float * __restrict__ B , 
-//	  float * __restrict__ C ) { 
-//#pragma clang loop vectorize(enable) 
-for (int i = 0; i < 1024; ++i)
-    C[i] = (A[i] * B[i]) + K;
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
+#include <assert.h>
+#include "_complex.h"
+
+#define TRAINING_BLOCK_SIZE (16)
+#define N_RANGE (8)
+void top_func(_complex<float> * __restrict__ a, float *__restrict__ b, _complex<float> * __restrict__ result){
+
+    int i,k;
+    _complex<float> accum(0.0f,0.0f);
+
+    for (i = 0; i < TRAINING_BLOCK_SIZE; i++)
+    {
+        accum = {0, 0};
+        for (k = 0; k < N_RANGE; k++)
+        {
+            const _complex<float> prod =
+                cconj(a[k])* sinf(b[k]);
+            accum += prod;
+        }
+        result[i] = accum;
     }
+}
