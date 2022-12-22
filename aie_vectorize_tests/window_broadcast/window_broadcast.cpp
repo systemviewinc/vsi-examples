884eb1c03b4ad0f155c774122c49dfa87026f77c
Tom Sechrist <thomas@systemviewinc.com>
2022-12-22 15:40:09 -0800

    Adding window_broadcast test

---
+++ aie_vectorize_tests/window_broadcast/window_broadcast.cpp
@@ -0,0 +1,38 @@
+#include <adf.h>
+#include <aie_api/aie.hpp>
+#include <aie_api/aie_adf.hpp>
+
+void passthrough(
+    input_window_float * in1, 
+    output_window_float * outp) 
+{
+    float c1;
+    for(int i = 0; i < 16*256; i++){
+        c1 = window_readincr(in1);
+        window_writeincr(outp, c1);
+    }
+}
+
+void simple_add(
+    input_window_float * in1, 
+    output_window_float * outp) 
+{
+    float c1;
+    for(int i = 0; i < 16*256; i++){
+        c1 = window_readincr(in1);
+        // float temp = static_cast<float>(i);
+        // c1 = c1 + temp;
+        window_writeincr(outp, (c1 + c1));
+    }
+}
+
+void simple_mul(
+    input_window_float * in1, 
+    output_window_float * outp) 
+{
+    float c1;
+    for(int i = 0; i < 16*256; i++){
+        c1 = window_readincr(in1);
+        window_writeincr(outp, (c1 * c1));
+    }
+}
