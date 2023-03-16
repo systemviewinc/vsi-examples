cb2767aa51c6c7b261773df324cdd3c8e2df24be
Somayeh Rahimipour <somayeh@systemviewinc.com>
2023-03-16 11:26:14 -0700

    increasing the loop size of vector_complex_reduction_new

--- aie_vectorize_tests/vector_complex_reduction_new/vector_complex_reduction_new.cc
+++ aie_vectorize_tests/vector_complex_reduction_new/vector_complex_reduction_new.cc
@@ -7,7 +7,7 @@ void complexReduction(float * __restrict__ A, float * __restrict__ C) {
 
     complex<float> accum(0.0f,0.0f);
     #pragma clang loop vectorize(enable)            
-    for (int i = 0; i < 8; ++i)
+    for (int i = 0; i < 64; ++i)
     {
         accum += Ai[i];
     }
