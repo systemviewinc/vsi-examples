59766d8ab114b35fef5a5c761a80bf682a16c19e
lida <lida@systemviewinc.com>
2023-04-25 11:08:44 -0700

    #534 fix aie_vectorize test

--- lab/aie_vectorizing/test.cc
+++ lab/aie_vectorizing/test.cc
@@ -2,21 +2,22 @@
 #include <math.h>
 #include <stdio.h>
 #include <assert.h>
-#include "_complex.h"
+#include "complex.h"
+using namespace std;
 
 #define TRAINING_BLOCK_SIZE (16)
 #define N_RANGE (8)
-void top_func(_complex<float> * __restrict__ a, float *__restrict__ b, _complex<float> * __restrict__ result){
+void top_func(complex<float> * __restrict__ a, float *__restrict__ b, complex<float> * __restrict__ result){
 
     int i,k;
-    _complex<float> accum(0.0f,0.0f);
+    complex<float> accum(0.0f,0.0f);
 
     for (i = 0; i < TRAINING_BLOCK_SIZE; i++)
     {
         accum = {0, 0};
         for (k = 0; k < N_RANGE; k++)
         {
-            const _complex<float> prod =
+            const complex<float> prod =
                 conj(a[k+i*N_RANGE]) * sinf(b[k+i*N_RANGE]);
             accum += prod;
         }
