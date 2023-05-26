14ff178ccead0a56100400e59bc92ed8a4433a51
lida <lida@systemviewinc.com>
2023-05-26 14:12:58 -0700

    add scalar fiexedcomplex multiplication test

---
+++ aie_vectorize_tests/scalar_fixedcomplex_mul/scalar_fixedcomplex_mul.cc
@@ -0,0 +1,18 @@
+#include "complex.h"
+using namespace std;
+
+void scalar_fixedcomplex_mul(int32_t * __restrict__ A, int32_t * __restrict__ B, int32_t * __restrict__ C0, int32_t * __restrict__ C1) {
+	complex<ap_int<1,15>> * __restrict Ai = (complex<ap_int<1,15>> * __restrict__)A;
+	complex<ap_int<1,15>> * __restrict Bi = (complex<ap_int<1,15>> * __restrict__)B;
+	complex<ap_int<1,15>> * __restrict C0o = (complex<ap_int<1,15>> * __restrict__)C0;
+	complex<ap_int<1,15>> * __restrict C1o = (complex<ap_int<1,15>> * __restrict__)C1;
+	ap_int<1,15> coeff(16384);
+#pragma clang loop vectorize(enable) //interleave_count(2)
+	for (int i = 0 ; i < 512; i++)
+		C0o[i] = Ai[i] * Bi[i];
+
+#pragma clang loop vectorize(enable) //interleave_count(2)
+	for (int i = 0 ; i < 512; i++)
+		C1o[i] = Ai[i] * coeff;
+
+}
