661ed9eaabed0833641c4d90abb42dbeb6933529
lida <lida@systemviewinc.com>
2023-06-07 09:02:09 -0700

    adding sin test for fixed-point type

--- aie_vectorize_tests/scalar_fixedq1.15_sin/scalar_fixed_sin.cc
+++ aie_vectorize_tests/scalar_fixedq1.15_sin/scalar_fixed_sin.cc
@@ -4,7 +4,7 @@
 void scalar_fixed_sin(ap_int<1,15> * __restrict__ A, 
 					ap_int<1,15> * __restrict__ C) {
 #pragma clang loop vectorize(disable) //interleave_count(1)
-	for (int i = 0 ; i < 1024; i++) {
+	for (int i = 0 ; i < 512; i++) {
           C[i] = A[i].sin();  // fixed sin
 	} 		 
 }
